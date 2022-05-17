e1d052c66f1dbf7fbda83befb8408c3776243292
mugishap <precieuxmugisha@gmail.com>
2022-05-17 19:07:03 +0200

    change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,9 +4,9 @@ import boxicons from "boxicons/css/boxicons.css";
 function Sidebar() {
   return (
     <div className="h-screen w-2/12 fixed flex flex-col text-white sidebar box-border pt-1 pr-1 pb-1 text:white">
-      <span className="text-2xl">Foodie</span>
+      <span className="text-4xl menu-logo">F<span className="text-4xl menu-logo oo">oo</span>die</span>
       <div className="mt-10 flex items-center justify-center flex-col w-full">
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-600 pr-7 pl-7">
+        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
           <div className="flex items-center justify-start w-1/2 h-full">
             <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
             <span className="bx bx-list-ul bx-md"></span>
@@ -15,7 +15,7 @@ function Sidebar() {
             <span>Overview</span>
           </div>
         </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-600 pr-7 pl-7">
+        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
           <div className="flex items-center justify-start w-1/2 h-full">
             <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
             <span className="bx bx-list-ul bx-md"></span>
@@ -24,7 +24,7 @@ function Sidebar() {
             <span>Tables</span>
           </div>
         </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-600 pr-7 pl-7">
+        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
           <div className="flex items-center justify-start w-1/2 h-full">
             <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
             <span className="bx bx-list-ul bx-md"></span>
@@ -33,7 +33,7 @@ function Sidebar() {
             <span>Orders</span>
           </div>
         </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-600 pr-7 pl-7">
+        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
           <div className="flex items-center justify-start w-1/2 h-full">
             <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
             <span className="bx bx-list-ul bx-md"></span>
