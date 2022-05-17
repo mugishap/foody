c8c57acf2d6ce0e15cb3c0ceb9e68f03ab9401f8
mugishap <precieuxmugisha@gmail.com>
2022-05-17 20:17:58 +0200

    merge

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import boxicons from "boxicons/css/boxicons.css";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   return (
@@ -8,62 +9,74 @@ function Sidebar() {
         F<span className="text-4xl menu-logo oo">oo</span>die
       </span>
       <div className="mt-20 mb-96 flex items-center justify-center flex-col w-full">
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bx-list-ul bx-md"></span>
+        <Link to='/signup' className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bx-list-ul bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Overview</span>
+            </div>
           </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Overview</span>
+        </Link>
+        <Link to="/signup" className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bxs-bowl-rice bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Tables</span>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bxs-bowl-rice bx-md"></span>
+        </Link>
+        <Link to="/signup" className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bxs-group bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Orders</span>
+            </div>
           </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Tables</span>
+        </Link>
+        <Link to="/signup" className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bxs-food-menu bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Menu</span>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bxs-group bx-md"></span>
-          </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Orders</span>
-          </div>
-        </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bxs-food-menu bx-md"></span>
-          </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Menu</span>
-          </div>
-        </div>
+        </Link>
       </div>
       <div className="mt-32 flex flex-col items-center justify-center">
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bxs-cog bx-md"></span>
+        <Link to="/signup" className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bxs-cog bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Settings</span>
+            </div>
           </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Settings</span>
+        </Link>
+        <Link to="/signup" className="w-full">
+          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center justify-start w-1/3 h-full">
+              <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
+              <span className="bx bxs-face bx-md"></span>
+            </div>
+            <div className="text-left flex items-center justify-start w-1/2">
+              <span>Account</span>
+            </div>
           </div>
-        </div>
-        <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-          <div className="flex items-center justify-start w-1/3 h-full">
-            <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-            <span className="bx bxs-face bx-md"></span>
-          </div>
-          <div className="text-left flex items-center justify-start w-1/2">
-            <span>Account</span>
-          </div>
-        </div>
+        </Link>
       </div>
     </div>
   );
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,30 +1,12 @@
 import React, { useState, useEffect } from "react";
+import Menuside from "../components/Menuside";
 import Sidebar from "../components/Sidebar";
 
 function Menu() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    getItems();
-  }, []);
-  const getItems = async () => {
-    const token = localStorage.getItem("token");
-    if (!token) return;
-    const api = await fetch(
-      "http://196.223.240.154:8099/supapp/api/menu-items?page=0&size=30",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + token,
-        },
-      }
-    );
-    const data = await api.json();
-    console.log(data);
-  };
   return (
-    <div>
+    <div className="flex justify-between">
       <Sidebar />
+      <Menuside />
     </div>
   );
 }
