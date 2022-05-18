835d24cbeeeb1cd11cebdde7de9b653cf8f7fec3
mugishap <precieuxmugisha@gmail.com>
2022-05-18 20:23:20 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Menu from "./pages/Menu";
 import Account from "./pages/Account";
+import Orders from "./pages/Orders";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route element={<Signup />} path="/signup"></Route>
         <Route element={<Menu />} path="/menu"></Route>
         <Route element={<Account />} path="/account"></Route>
+        <Route element={<Orders/>} path="/orders"></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 function Sidebar(props) {
   let tab = props.tab;
   console.log(tab);
+  setTimeout(() => {
+    document.querySelector(".bg-blue-500").parentElement.style.color = "white";
+  }, 100);
   return (
     <div className="sidebar-div h-screen w-2/12 fixed flex flex-col text-white sidebar box-border pt-1 pr-1 pb-1 text:white">
       <span className="text-4xl menu-logo">
@@ -12,108 +15,132 @@ function Sidebar(props) {
       </span>
       <div className="mt-20 mb-96 flex items-center justify-center flex-col w-full">
         <Link to="/overview" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "overview" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }
-              <span className="bx bx-list-ul bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bx-list-ul bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Overview</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Overview</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bx-list-ul bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Overview</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
         <Link to="/tables" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "tables" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }{" "}
-              <span className="bx bxs-bowl-rice bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bxs-bowl-rice bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Tables</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Tables</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bxs-bowl-rice bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Tables</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
         <Link to="/orders" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "orders" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }{" "}
-              <span className="bx bxs-group bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bxs-group bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Orders</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Orders</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bxs-group bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Orders</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
         <Link to="/menu" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "menu" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }{" "}
-              <span className="bx bxs-food-menu bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bxs-food-menu bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Menu</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Menu</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bxs-food-menu bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Menu</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
       </div>
       <div className="mt-32 flex flex-col items-center justify-center">
         <Link to="/settings" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "settings" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }{" "}
-              <span className="bx bxs-cog bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bxs-cog bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Settings</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Settings</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bxs-cog bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Settings</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
         <Link to="/account" className="w-full">
-          <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 text-gray-400 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
-            <div className="flex items-center justify-start w-1/3 h-full">
-              {
-                (tab === "account" ? (
-                  <div className="bg-blue-500 focused-div mr-4 w-1/12 h-full"></div>
-                ) : (
-                  <div className="bg-transparent focused-div mr-4 w-1/12 h-full"></div>
-                ))
-              }{" "}
-              <span className="bx bxs-face bx-md"></span>
+          {tab === "overview" ? (
+            <div className="border-l-white flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full">
+                <span className="bx bx-list-ul bx-md"></span>
+              </div>
+              <div className="text-left text-white flex items-center justify-start w-1/2">
+                <span>Account</span>
+              </div>
             </div>
-            <div className="text-left flex items-center justify-start w-1/2">
-              <span>Account</span>
+          ) : (
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+              <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
+                <span className="bx bxs-face bx-md"></span>
+              </div>
+              <div className="text-left text-gray-500 flex items-center justify-start w-1/2">
+                <span>Account</span>
+              </div>
             </div>
-          </div>
+          )}
         </Link>
       </div>
     </div>
