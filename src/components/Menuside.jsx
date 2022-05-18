cbc2dad42fa26fd3eebb341ee3f55b99713f91b4
mugishap <precieuxmugisha@gmail.com>
2022-05-18 08:56:02 +0200

    save

--- src/components/Menuside.jsx
+++ src/components/Menuside.jsx
@@ -4,9 +4,48 @@ import moment from "moment";
 
 function Menuside() {
   const [items, setItems] = useState([]);
+  const [loader, setLoader] = useState(true);
+  const [images, setImages] = useState([]);
   useEffect(() => {
     getItems();
   }, []);
+
+  const getImages = async () => {
+    const names = items.content.map((item) => {
+      return item.name.split(" ")[1];
+    });
+    if (!names) return;
+    let imagesFromLocalStorage = JSON.parse(localStorage.getItem("images"));
+    if (imagesFromLocalStorage) {
+      setImages(imagesFromLocalStorage);
+      console.log(images);
+      setLoader(false);
+    } else {
+      for (let i = 0; i < names.length; i++) {
+        const api = await fetch(
+          `https://bing-image-search1.p.rapidapi.com/images/search?q=${names[i]}`,
+          {
+            method: "GET",
+            headers: {
+              "X-RapidAPI-Host": "bing-image-search1.p.rapidapi.com",
+              "X-RapidAPI-Key":
+                "4d3efa6d60mshe9647b4fc7ea6dbp1bf6dajsn7de302358e22",
+            },
+          }
+        );
+        const data = await api.json();
+        let imageArray = [];
+        imageArray.push({
+          key: items.content[i].createdAt,
+          url: data.value[0].thumbnailUrl,
+        });
+        console.log(imageArray);
+        setImages(imageArray);
+      }
+      localStorage.setItem("images", JSON.stringify(images));
+      setLoader(false);
+    }
+  };
   const getItems = async () => {
     const token = localStorage.getItem("token");
     if (!token) return;
@@ -23,9 +62,10 @@ function Menuside() {
     const data = await api.json();
     console.log(data);
     setItems(data);
+
+    getImages();
   };
   const date = moment().format("dddd, MMMM Do YYYY");
-  console.log(date);
   return (
     <div className="ml-72 w-10/12 flex flex-col items-center">
       <Navbar />
@@ -93,15 +133,27 @@ function Menuside() {
             </button>
           </div>
         </div>
-        {items.content &&
-          items.content.map((item) => {
-            return (
-              <div className="w-10/12 h-48 m-3 rounded-xl bg-gray-200 flex flex-col ">
-                <span>{item.description}</span>
-                <span>{item.name}</span>
-              </div>
-            );
-          })}
+        {!loader ? (
+          <>
+            {items.content &&
+              items.content.map((item) => {
+                let i = 0;
+                return (
+                  <div
+                    key={item.createdAt}
+                    className="w-10/12 h-48 m-3 rounded-xl bg-gray-200 flex flex-col justify-center"
+                  >
+                    <img src={images[i].url} className="w-24 h-24" alt="" />
+                    <span>{item.description}</span>
+                    <span>{item.name}</span>
+                    <span>{item.price}</span>
+                  </div>
+                );
+              })}
+          </>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
