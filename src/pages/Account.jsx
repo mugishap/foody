0605a782d0f2e7c917bab47d2ea02f606f6114cc
mugishap <precieuxmugisha@gmail.com>
2022-05-20 05:51:15 +0200

    top

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
@@ -7,35 +8,71 @@ function Account() {
 
   const [user, setUser] = useState({});
   const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    JSON.parse(localStorage.getItem("userCredentials")) === {}
+      ? window.location.replace("/login")
+      : setUser(JSON.parse(localStorage.getItem("userCredentials")));
+  }, []);
+const handleUpdate = (e) => {
+  e.preventDefault()
+  swa
+}
   return (
     <div className="flex justify-between  items-stretch flex-row">
       <Sidebar tab="account" />
       <div className="w-10/12  fixed overflow-hidden z-10 flex flex-col items-center justify-center">
-        <Navbar />
         <div className="m-24 flex flex-col justify-center items-center bg-slate-300 rounded-md w-6/12 box-border  h-auto p-10">
           <img
-            src="https://tse2.mm.bing.net/th?id=OIP.UFzzd3TPwLbF6giTYKpJogHaHa&pid=Api"
+            src="https://res.cloudinary.com/precieux/image/upload/v1652201747/avatar-man_fzclmz.png"
             className="w-32 rounded-full h-32"
             alt=""
           />
-          <div className="form w-8/12 flex flex-col justify-center">
-            <form className="w-full">
-              <div className="labels flex justify-between items-center w-full">
-                <label htmlFor="username">Username: </label>
-                <input type="text" className=" p-1 w-2/3" disabled={true} value={"Hello"} />
+          <div className="form w-8/12 flex flex-col justify-center items-center">
+            <form className="w-full flex flex-col items-center justify-center ">
+              <div className="labels mt-3 flex justify-between items-center w-full">
+                <label htmlFor="username">First Name: </label>
+                <input
+                  type="text"
+                  className=" p-2 rounded bg-transparent w-2/3 "
+                  disabled={true}
+                  value={user.firstName}
+                />
               </div>
-              <div className="labels flex justify-between items-center w-full">
-                <label htmlFor="username">Username: </label>
-                <input type="text" className=" p-1 w-2/3" disabled={true} value={"Hello"} />
+              <div className="labels mt-3 flex justify-between items-center w-full">
+                <label htmlFor="username">Last Name: </label>
+                <input
+                  type="text"
+                  className=" p-2 rounded bg-transparent w-2/3 "
+                  disabled={true}
+                  value={user.lastName}
+                />
               </div>
-              <div className="labels flex justify-between items-center w-full">
-                <label htmlFor="username">Username: </label>
-                <input type="text" className=" p-1 w-2/3" disabled={true} value={"Hello"} />
+              <div className="labels mt-3 flex justify-between items-center w-full">
+                <label htmlFor="username">Email: </label>
+                <input
+                  type="text"
+                  className=" p-2 rounded bg-transparent w-2/3 "
+                  disabled={true}
+                  value={user.email}
+                />
               </div>
-              <div className="labels flex justify-between items-center w-full">
-                <label htmlFor="username">Username: </label>
-                <input type="text" className=" p-1 w-2/3" disabled={true} value={"Hello"} />
+              <div className="labels mt-3 flex justify-between items-center w-full">
+                <label htmlFor="username">Phone: </label>
+                <input
+                  type="text"
+                  className=" p-2 rounded bg-transparent w-2/3 "
+                  disabled={true}
+                  value={user.phone}
+                />
               </div>{" "}
+            <Button
+              variant="contained"
+              onClick={handleUpdate}
+              className="w-3/5"
+              type="submit"
+            >
+              Edit profile
+            </Button>
             </form>
           </div>
         </div>
