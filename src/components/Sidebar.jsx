743aef724dd99338d94a9ab08879a03cb87c9dc1
mugishap <precieuxmugisha@gmail.com>
2022-05-20 09:46:44 +0200

    cont

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import boxicons from "boxicons/css/boxicons.css";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+import { VscAccount } from "react-icons/vsc";
 
 function Sidebar(props) {
   let tab = props.tab;
@@ -20,7 +21,7 @@ function Sidebar(props) {
       <div className="mt-16 mb-96 flex items-center justify-center flex-col w-full">
         <Link to="/overview" className="w-full">
           {tab == "overview" ? (
-            <div className="border-l-white border-l-4 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="border-l-white border-l-4 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full">
                 <span className="bx fixed absolute left-[30px] bx-list-ul bx-md"></span>
               </div>
@@ -29,7 +30,7 @@ function Sidebar(props) {
               </div>
             </div>
           ) : (
-            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
                 <span className="bx fixed bx-list-ul bx-md"></span>
               </div>
@@ -41,7 +42,7 @@ function Sidebar(props) {
         </Link>
         <Link to="/tables" className="w-full">
           {tab == "tables" ? (
-            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full">
                 <span className="bx fixed absolute left-[30px] bxs-bowl-rice bx-md"></span>
               </div>
@@ -50,7 +51,7 @@ function Sidebar(props) {
               </div>
             </div>
           ) : (
-            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
                 <span className="bx fixed bxs-bowl-rice bx-md"></span>
               </div>
@@ -62,7 +63,7 @@ function Sidebar(props) {
         </Link>
         <Link to="/orders" className="w-full">
           {tab == "orders" ? (
-            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full">
                 <span className="bx fixed absolute left-[30px] bxs-group bx-md"></span>
               </div>
@@ -71,7 +72,7 @@ function Sidebar(props) {
               </div>
             </div>
           ) : (
-            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
                 <span className="bx fixed bxs-group bx-md"></span>
               </div>
@@ -83,7 +84,7 @@ function Sidebar(props) {
         </Link>
         <Link to="/menu" className="w-full">
           {tab == "menu" ? (
-            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full">
                 <span className="bx fixed absolute left-[30px] bxs-food-menu bx-md"></span>
               </div>
@@ -92,7 +93,7 @@ function Sidebar(props) {
               </div>
             </div>
           ) : (
-            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
                 <span className="bx fixed bxs-food-menu bx-md"></span>
               </div>
@@ -106,7 +107,7 @@ function Sidebar(props) {
       <div className=" flex flex-col items-center justify-center">
         <Link to="/account" className="w-full">
           {tab == "account" ? (
-            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="border-l-white border-l-8 flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full">
                 <span className="bx fixed absolute left-[30px] bxs-face bx-md"></span>
               </div>
@@ -115,7 +116,7 @@ function Sidebar(props) {
               </div>
             </div>
           ) : (
-            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-700 hover:text-white pr-7 pl-7">
+            <div className="flex items-center flex-row justify-between w-full box-border m-1 font-semibold text-xl h-10 cursor-pointer hover:bg-slate-600 rounded-xl hover:text-white pr-7 pl-7">
               <div className="flex items-center justify-start w-1/3 h-full text-gray-500">
                 <span className="bx fixed bxs-face bx-md"></span>
               </div>
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/Sidebar";
 import "../App.css";
 import Navbar from "../components/Navbar";
@@ -6,8 +6,13 @@ import Order from "../components/Order";
 import OrderStats from "../components/OrderStats";
 import { AiFillPlusCircle } from "react-icons/ai";
 import OrderType from "../components/OrderType";
+import moment from "moment";
 
 function Orders() {
+  const [data, setData] = useState({});
+  const date = moment().format("dddd, MMMM Do YYYY");
+  const [loader, setLoader] = useState(true);
+
   const getOrders = async () => {
     const token = localStorage.getItem("accessToken");
     if (!token) return window.location.replace("/login");
@@ -22,169 +27,93 @@ function Orders() {
         },
       }
     );
-    const data = await api.json()
-    console.log(data)
+    const data = await api.json();
+    console.log(data);
+    setData(data.content);
+    setLoader(false)
   };
-  const orderstats = [
-    {
-      status: "delivered",
-      number: 6,
-    },
-    {
-      status: "Waiting",
-      number: 12,
-    },
-    {
-      status: "Rejected",
-      number: 1,
-    },
-    {
-      status: "All",
-      number: 30,
-    },
-  ];
-  const order = [
-    {
-      no: 1,
-      orderName: "Kaffir Lime vodka lemongreass,Ginger,citrus",
-      recipe: "Tommy yummy",
-      qty: 2,
-      tableName: "Table 8",
-      amount: 5000,
-      clientType: "Guest",
-      phoneNo: "0788457865",
-    },
-    {
-      no: 2,
-      orderName: "Kaffir Lime vodka lemongreass,Ginger,citrus",
-      recipe: "Tommy yummy",
-      qty: 2,
-      tableName: "Table 8",
-      amount: 5000,
-      clientType: "Guest",
-      phoneNo: "0788457865",
-    },
-    {
-      no: 3,
-      orderName: "Kaffir Lime vodka lemongreass,Ginger,citrus",
-      recipe: "Tommy yummy",
-      qty: 2,
-      tableName: "Table 8",
-      amount: 5000,
-      clientType: "Guest",
-      phoneNo: "0788457865",
-    },
-    {
-      no: 4,
-      orderName: "Kaffir Lime vodka lemongreass,Ginger,citrus",
-      recipe: "Tommy yummy",
-      qty: 2,
-      tableName: "Table 8",
-      amount: 5000,
-      clientType: "Guest",
-      phoneNo: "0788457865",
-    },
-    {
-      no: 5,
-      orderName: "Kaffir Lime vodka lemongreass,Ginger,citrus",
-      recipe: "Tommy yummy",
-      qty: 2,
-      tableName: "Table 8",
-      amount: 5000,
-      clientType: "Guest",
-      phoneNo: "0788457865",
-    },
-  ];
-  const ordertype = [
-    {
-      type: "Dessert",
-      state: "NEW",
-    },
-    {
-      type: "Main",
-      state: "NEW",
-    },
-    {
-      type: "Drink",
-      state: "NEW",
-    },
-    {
-      type: "Appetizer",
-      state: "NEW",
-    },
-    {
-      type: "Starter",
-      state: "NEW",
-    },
-  ];
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+
   return (
     <div className="order-main flex items-start justify-start  w-full">
       <Sidebar />
-      <div className="orders fixed w-10/12">
+      <div className="orders fixed flex items-center justify-center ml-[5 em] flex-col  w-10/12">
         <Navbar />
-        <div className="order-tab ml-[30vw]">
-          <div className="lfloaters">
-            <div className="uppers">
-              <div>
-                <h3>Orders</h3>
-                <p>as of 25 May 2022, 09:41 PM</p>
+        {loader ? (
+          <img src="https://res.cloudinary.com/precieux/image/upload/v1653023282/cube_wg1jwa.gif" className="w-1/12 m-auto" alt="" />
+        ) : (
+          <div className="order-tab  ml-[30vw]">
+            <div className="lfloaters">
+              <div className="uppers ">
+                <div className="">
+                  <h3>Orders</h3>
+                  <p>{date}</p>
+                </div>
+                <div className="btns">
+                  <button className="hover:bg-[#f53b57] hover:text-white">
+                    New
+                  </button>
+                  <button className="hover:bg-[#f53b57] hover:text-white">
+                    Delivered
+                  </button>
+                  <button className="hover:bg-[#f53b57] hover:text-white">
+                    Rejected
+                  </button>
+                  <button className="hover:bg-[#f53b57] hover:text-white">
+                    All
+                  </button>
+                </div>
               </div>
-              <div className="btns">
-                <button className="hover:bg-[#f53b57] hover:text-white">
-                  New
-                </button>
-                <button className="hover:bg-[#f53b57] hover:text-white">
-                  Delivered
-                </button>
-                <button className="hover:bg-[#f53b57] hover:text-white">
-                  Rejected
-                </button>
-                <button className="hover:bg-[#f53b57] hover:text-white">
-                  All
-                </button>
+              <div className="all-orders">
+                {data.map((item) => (
+                  <Order
+                    no={item.id}
+                    clientName={item.customer.firstName}
+                    orderName={item.orderDetails[0].item.name}
+                    recipe={item.orderDetails[0].item.name}
+                    qty={item.numberOfProducts}
+                    tableName={item.tableName}
+                    amount={item.orderDetails[0].item.unitPrice}
+                    clientType={item.serviceProvider.address}
+                    phoneNo={item.customer.mobile}
+                    key={item.createdAt}
+                  />
+                ))}
               </div>
             </div>
-            <div className="all-orders">
-              {order.map((item) => (
-                <Order
-                  no={item.no}
-                  orderName={item.orderName}
-                  recipe={item.recipe}
-                  qty={item.qty}
-                  tableNAme={item.tableName}
-                  amount={item.amount}
-                  clientType={item.clientType}
-                  phoneNo={item.phoneNo}
-                  key={item.no}
+            <div className="rfloaters">
+              {data.map((item) => (
+                <OrderStats
+                  status={item.status}
+                  number={item.numberOfProducts}
+                  key={item.createdAt}
                 />
               ))}
-            </div>
-          </div>
-          <div className="rfloaters">
-            {orderstats.map((item) => (
-              <OrderStats
-                status={item.status}
-                number={item.number}
-                key={item.number}
-              />
-            ))}
-            <div className="add-order">
-              <h3>Add Order</h3>
-              <p>Manually</p>
-              <div className="field">
-                <input  onChange={()=>{}} type="text" value="Create new order" />
-                <AiFillPlusCircle className="icon" />
+              <div className="add-order">
+                <h3>Add Order</h3>
+                <p>Manually</p>
+                <div className="field">
+                  <input
+                    onChange={() => {}}
+                    type="text"
+                    value="Create new order"
+                  />
+                  <AiFillPlusCircle className="icon" />
+                </div>
+                {data.map((item) => (
+                  <OrderType
+                    type={item.orderType}
+                    state={item.state}
+                    key={item.type}
+                  />
+                ))}
               </div>
-              {ordertype.map((item) => (
-                <OrderType
-                  type={item.type}
-                  state={item.state}
-                  key={item.type}
-                />
-              ))}
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
